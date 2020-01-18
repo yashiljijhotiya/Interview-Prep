481eb9be6b5528cee8537188cb3476a2e234de73
yashil-jijhotiya <yashil.jijhotiya@rapidd.net>
2020-01-18 19:53:25 +0530

    java8 practice

--- src/Java8/HashMapOperations.java
+++ src/Java8/HashMapOperations.java
@@ -11,21 +11,20 @@ import static java.util.Map.Entry.comparingByValue;
 public class HashMapOperations {
 
     public static void main(String[] args) {
-        Employee ey = new Employee(17,"Yashil",70000,"Fullstack developer");
-        Employee ea = new Employee(21,"Abinash",40000,"Tester");
-        Employee eb = new Employee(13,"Biswajeet",60000,"Web developer");
-        Employee es = new Employee(7,"Shahi",60000,"Backend developer");
+        Employee ey = new Employee(17, "Yashil", 70000, "Fullstack developer");
+        Employee ea = new Employee(21, "Abinash", 40000, "Tester");
+        Employee eb = new Employee(13, "Biswajeet", 60000, "Web developer");
+        Employee es = new Employee(7, "Shahi", 60000, "Backend developer");
 
         HashMap<Integer, Employee> map = new HashMap<>();
-        map.put(ey.id,ey);
-        map.put(es.id,es);
-        map.put(ea.id,ea);
-        map.put(eb.id,eb);
+        map.put(ey.id, ey);
+        map.put(es.id, es);
+        map.put(ea.id, ea);
+        map.put(eb.id, eb);
 //        Map<Integer, Employee> sortedMap = map.entrySet().stream().
 //                   collect(Collectors.toMap(k -> k.getKey(), e -> e.getValue())).entrySet().stream().sorted(Map.Entry.comparingByKey()).
 //                   collect(Collectors.toMap(Map.Entry :: getKey, Map.Entry :: getValue, (oldVal, newVal) -> oldVal, LinkedHashMap::new));
 //        sortedMap.forEach((k,v) -> System.out.println(k + " : " + v));
-
 
 
         ArrayList<Employee> list = new ArrayList<>();
@@ -35,7 +34,7 @@ public class HashMapOperations {
         list.add(eb);
 
         // for sorting list on various Comparator
-        Comparator<Employee> nameComparator = (e1, e2) ->{
+        Comparator<Employee> nameComparator = (e1, e2) -> {
             return e1.getName().compareTo(e2.getName());
         };
 
@@ -43,7 +42,7 @@ public class HashMapOperations {
             return e1.salary - e2.getSalary();
         };
 
-        Comparator<Employee> positionComparator = (e1, e2)->{
+        Comparator<Employee> positionComparator = (e1, e2) -> {
             return e1.getPosition().compareTo(e2.getPosition());
         };
 
@@ -51,8 +50,8 @@ public class HashMapOperations {
         Collections.sort(list, positionComparator);
         Collections.sort(list, salaryComparator);
 
-        list.forEach(val ->{
-            System.out.println("Id: " +val.getId() + "name: " +val.getName() + "salary :" + val.getSalary() + "posistion : " + val.getPosition());
+        list.forEach(val -> {
+            System.out.println("Id: " + val.getId() + "name: " + val.getName() + "salary :" + val.getSalary() + "posistion : " + val.getPosition());
         });
 
         TreeMap<Employee, Integer> treeMap = new TreeMap<>(new Comparator<Employee>() {
@@ -61,22 +60,21 @@ public class HashMapOperations {
                 return o1.getName().compareTo(o2.getName());
             }
         });
-        treeMap.put(ey,ey.id);
-        treeMap.put(es,es.id);
-        treeMap.put(eb,eb.id);
-        treeMap.put(ea,ea.id);
+        treeMap.put(ey, ey.id);
+        treeMap.put(es, es.id);
+        treeMap.put(eb, eb.id);
+        treeMap.put(ea, ea.id);
 
 
-        System.out.println("treemap : " +treeMap);
+        System.out.println("treemap : " + treeMap);
 
         //pq sorted by name
-        PriorityQueue<Employee> pq = new PriorityQueue<>((e1,e2) -> e1.getName().compareTo(e2.getName()));
+        PriorityQueue<Employee> pq = new PriorityQueue<>((e1, e2) -> e1.getName().compareTo(e2.getName()));
         pq.add(ey);
         pq.add(eb);
         pq.add(es);
         pq.add(ea);
         //System.out.println("priority queue : " +pq);
-
 
 
     }
